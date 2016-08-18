d597786425f82b419c1f1c128d2fcd0fc8180662
Steven Shaw <steven@steshaw.org>
2016-08-18 21:10:35 +1000

    Post JSON rather than form

--- app/Main.hs
+++ app/Main.hs
@@ -5,16 +5,17 @@ module Main where
 import Control.Concurrent.Async
 import Control.Lens
 import Data.Aeson
+import Data.Aeson.Lens (key)
 import Data.Foldable
 import Network.Wreq
 
-postTest s = do
-  r <- post "http://httpbin.org/post" (partText "q" s)
-  let Just obj = decode (r ^. responseBody)
-  pure (obj :: Object)
+echo :: Integer -> IO (Maybe Value)
+echo s = do
+  r <- post "http://httpbin.org/post" (toJSON s)
+  pure $ r ^? responseBody . key "json"
 
-posts = map postTest ["Hello", "There", "Foo", "Bar"]
+posts = map echo [1..10]
 
 main = do
-  a <- runConcurrently $ asum (map Concurrently posts)
-  print a
+  first <- runConcurrently $ asum (map Concurrently posts)
+  print first
